1c618b8befc04a6ec60106fe65fc144549165476
Vincent <vincentyan8@hotmail.com>
2020-01-22 15:59:04 -0800

    What: Graph is now dynamic, takes in arguments and a thread controls the drawing of the figures and updates it.
    Changed functionality of the plot button, it hides and shows the graph (as described on Canvas)

--- Lab_2.py
+++ Lab_2.py
@@ -3,7 +3,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-#from gpiozero import MCP3008
+# from gpiozero import MCP3008
 import threading
 import time
 import random
@@ -14,40 +14,47 @@ def setTemp(temp):
     therm["value"] = temp
 
 
+def plotOnOff():
+    global plotVar
+    if plotVar:
+        plotVar = False
+    else:
+        plotVar = True
+
+
 '''Functions for buttons and initializations'''
 
 
 # Function for plot button
 def plotTemperature():
-    plt.close()
-    sum = 0
-    for i in temperatureQueue:
-        sum += i
-    avgTemp = sum / 10
-    avgQueue = [avgTemp] * 10
-    plt.plot([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], temperatureQueue)
-    plt.ylabel("Temperature (Celcius)")
-    plt.xlabel("Last 10 readings")
-    plt.title("Temperature Plot")
-    plt.plot([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], avgQueue, label='Average Temperature')
-    plt.legend(loc="upper left")
-    plt.show()
-
-
-def plotLuminescence():
-    plt.close()
-    sum = 0
-    for i in lumQueue:
-        sum += i
-    avgLum = sum / 10
-    avgQueue = [avgLum] * 10
-    plt.plot([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], temperatureQueue)
-    plt.ylabel("Luminescence (Lum)")
-    plt.xlabel("Last 10 readings")
-    plt.title("Luminescence Plot")
-    plt.plot([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], avgQueue, label='Average Luminescence')
-    plt.legend(loc="upper left")
-    plt.show()
+    global plotVar
+    while True:
+        while plotVar:
+            tmpRev = list(reversed(temperatureQueue))
+            lumRev = list(reversed(lumQueue))
+            color = 'tab:red'
+            color2 = 'tab:blue'
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            plt.title("Temperature and Luminescence Plot")
+            Ln, = ax.plot(tmpRev)
+            ax.set_xlim([0, 10])
+            ax.set_ylim([0, 100])
+            ax.set_xlabel('Readings')
+            ax.set_ylabel('Temperature (*C)', color=color2)
+            ax2 = ax.twinx()
+            ax2.set_ylabel('Luminescence', color=color)
+            ax2.set_ylim([0, 255])
+            Ln2, = ax2.plot(lumRev, color=color)
+            plt.ion()
+            while plotVar:
+                plt.show()
+                Ln.set_ydata(list(reversed(temperatureQueue)))
+                Ln2.set_ydata(list(reversed(lumQueue)))
+                Ln.set_xdata(range(len(temperatureQueue)))
+                Ln2.set_xdata(range(len(temperatureQueue)))
+                plt.pause(0.1)
+            plt.close()
 
 
 # Change the values to values of the photocell and lm 35 data
@@ -57,36 +64,43 @@ def getTemp():
     global lumQueue
     while True:
         while readTemp:
-            temperatureQueue.pop()  # Pops the last element
-            temperatureQueue.insert(0, random.randint(0,
-                                                      100))  # Pushes the first element into Queue, replace with lm35.value
-            lumQueue.pop()
-            lumQueue.insert(0, random.randint(0, 100))
-            setTemp(temperatureQueue[0])
+            if len(temperatureQueue) < 10:
+                temperatureQueue.insert(0, random.randint(0, 100))
+                lumQueue.insert(0, random.randint(0, 255))
+            else:
+                temperatureQueue.pop()  # Pops the last element
+                temperatureQueue.insert(0, random.randint(0,
+                                                          100))  # Pushes the first element into Queue, replace with lm35.value
+                lumQueue.pop()
+                lumQueue.insert(0, random.randint(0, 100))
+                setTemp(temperatureQueue[0])
             if celcius:
                 tempLabel["text"] = str(temperatureQueue[0]) + " *C"
                 lumLabel["text"] = str(lumQueue[0])
-                avgTempLabel["text"] = str(round(sum(temperatureQueue.__iter__())/10)) + " *C"
-                avgLumLabel["text"] = str(round(sum(lumQueue.__iter__())/10))
+                avgTempLabel["text"] = str(round(sum(temperatureQueue.__iter__()) / len(lumQueue))) + " *C"
+                avgLumLabel["text"] = str(round(sum(lumQueue.__iter__()) / len(lumQueue)))
             else:
                 tempLabel["text"] = str(round(temperatureQueue[0] * 1.8 + 32)) + " *F"
                 lumLabel["text"] = str(lumQueue[0])
                 avgTempLabel["text"] = str(round(sum(temperatureQueue.__iter__()) / 10)) + " *F"
                 avgLumLabel["text"] = str(round(sum(lumQueue.__iter__()) / 10))
-
-            print("Temperature Queue: " + str(temperatureQueue))
-            print("Luminescence Queue: " + str(lumQueue))
             time.sleep(float(readTime))
 
-#Sets the temperature value on the thermometer
+
+# Sets the temperature value on the thermometer
 def setTemp(temp):
-    therm["value"]=temp
+    therm["value"] = temp
+
 
 # Initialization function for daemon thread to read values
 def __init__():
     thread = threading.Thread(target=getTemp, args=())
     thread.daemon = True
     thread.start()
+    thread2 = threading.Thread(target=plotTemperature, args=())
+    thread2.daemon = True
+    thread2.start()
+
 
 
 # Function to start and stop daemon thread readout
@@ -134,6 +148,7 @@ def StartProgram():
     global start
     start = True
 
+
 def exitConfirm():
     windowExit = Tk()
     windowExit.title("Exit Program")
@@ -142,17 +157,14 @@ def exitConfirm():
     Button(windowExit, text="No", command=windowExit.destroy).grid(row=1, column=2, sticky="W")
 
 
-
 # Initialization Variables
 readTime = 2
 readTemp = False
 start = False
 celcius = True
-temperatureQueue = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-lumQueue = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-
-
+plotVar = False
+temperatureQueue = []
+lumQueue = []
 
 '''Program Starts Here'''
 StartWindow = Tk()
@@ -168,12 +180,12 @@ if not start:
 '''Start of the main program'''
 
 window = Tk()
+
 window.geometry("800x450")
 __init__()  # Initializes a daemon thread to read values off sensors (runs in the background, collects data)
-matplotlib.use('TkAgg')
 window.title("Group 20's Temperature and Light Sensor Program")  # sets title
 
-welcome = Label(window, text="Welcome to our program!", fg = "blue", font=("Calibri", 25))
+welcome = Label(window, text="Welcome to our program!", fg="blue", font=("Calibri", 25))
 start = Label(window, text="To start sensor reading, set a read time \n(default is 2 seconds) and press Start Reading")
 blank = Label(window, text="\n\n").grid(row=1, column=1)
 currTemp = Label(window, text="Current Temperature: ")
@@ -189,10 +201,10 @@ timeEntry.grid(row=3, column=1, sticky="E", padx=5, pady=5)
 currTemp.grid(row=6, column=1, sticky="W", padx=3, pady=3)
 currLum.grid(row=7, column=1, sticky="W", padx=3, pady=3)
 
-tempLabel = Label(window, text=str(temperatureQueue[0]) + " *C")
+tempLabel = Label(window, text=" *C")
 tempLabel.grid(row=6, column=1, sticky="E", padx=3, pady=3)
 
-lumLabel = Label(window, text=str(lumQueue[0]))
+lumLabel = Label(window, text=" ")
 lumLabel.grid(row=7, column=1, sticky="E", padx=3, pady=3)
 
 convertCF = Button(window, text="C/F", command=celToFah)
@@ -205,36 +217,34 @@ avgTempLabel.grid(row=8, column=1, sticky="W", padx=3, pady=3)
 avgLumLabel = Label(window, text="Average Luminescence: ")
 avgLumLabel.grid(row=9, column=1, sticky="W", padx=3, pady=3)
 
-
 avgTempLabel = Label(window, text="0 *C")
 avgTempLabel.grid(row=8, column=1, sticky="E", padx=3, pady=3)
 avgLumLabel = Label(window, text="0")
 avgLumLabel.grid(row=9, column=1, sticky="E", padx=3, pady=3)
 
-#Thermometer
-Label(window, text="50*C/112*F").grid(row = 4, column=3, columnspan = 2, padx = 5, pady = 1) #High temp label
+# Thermometer
+Label(window, text="50*C/112*F").grid(row=4, column=3, columnspan=2, padx=5, pady=1)  # High temp label
 s = ttk.Style()
 s.theme_use('clam')
 s.configure("red.Horizontal.TProgressbar", foreground='red', background='red')
-therm = ttk.Progressbar(window, style="red.Horizontal.TProgressbar", orient="vertical", length=200, mode="determinate", maximum=4, value=1) #Progress bar
-therm.grid(row=5, rowspan=4, column=3, columnspan = 2, padx=1, pady=1)
+therm = ttk.Progressbar(window, style="red.Horizontal.TProgressbar", orient="vertical", length=200, mode="determinate",
+                        maximum=4, value=1)  # Progress bar
+therm.grid(row=5, rowspan=4, column=3, columnspan=2, padx=1, pady=1)
 therm["maximum"] = 50
-Label(window, text="0*C/32*F").grid(row = 9, column=3, columnspan = 2, padx = 1, pady = 1) #Low temp label
+Label(window, text="0*C/32*F").grid(row=9, column=3, columnspan=2, padx=1, pady=1)  # Low temp label
 getTimeButton = Button(window, text="Set Time", command=getTime, width=10).grid(row=1, column=5, padx=5, pady=5)
 
 startTempButton = Button(window, text="Start Reading", command=StartTemp, width=20)
 stopTempButton = Button(window, text="Stop Reading", command=stopTemp, width=20)
 
-plotTempButton = Button(window, text="Temperature Plot", command=plotTemperature, width=20) # Opens a new window for plot data
-plotLumButton = Button(window, text="Luminescence Plot", command=plotLuminescence, width=20)
+plotTempButton = Button(window, text="Plots", command=plotOnOff,
+                        width=20)  # Opens a new window for plot data
 
 exitButton = Button(window, text="Exit", command=exit, width=20)
-
 
 startTempButton.grid(row=4, column=5, padx=5, pady=5)
 stopTempButton.grid(row=5, column=5, padx=5, pady=5)
 plotTempButton.grid(row=6, column=5, padx=5, pady=5)  # Opens a new window for plot data
-plotLumButton.grid(row=7, column=5, sticky="W", padx=5, pady=5)
 exitButton.grid(row=8, column=5, sticky="W", padx=5, pady=5, columnspan=3)
 
 window.mainloop()
